f65da5b137f4a7bfc6e8489bd5caef76f358ea63
s.kuebel <s.kuebel@otterbach.de>
2017-08-10 15:04:24 +0200

    Sortiert, aber leider Rückwerts

--- src/Klaus1Haupt.java
+++ src/Klaus1Haupt.java
@@ -3,7 +3,17 @@ public class Klaus1Haupt {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-
+		ListeF liste = new ListeF(6);
+		liste.sortedInsert(3);
+		liste.sortedInsert(5);
+		liste.sortedInsert(4);
+		liste.sortedInsert(95);
+		liste.sortedInsert(45);
+		liste.sortedInsert(1);
+		
+		
+		System.out.println(liste);
+		
 	}
 
 }
--- src/ListeF.java
+++ src/ListeF.java
@@ -9,13 +9,44 @@ public class ListeF {
 	}
 
 	public void sortedInsert(Comparable o) {
-		// sortiert einf ̈ugen
-		if(lastIndex >= element.length-1) {
+
+		//Comparable[] temp = new Comparable[element.length];
+
+		if (lastIndex >= element.length - 1) {
 			throw new RuntimeException("Liste Voll! Bitte keine weiteren Elemente mehr einfügen.");
 		} else {
-			
+			lastIndex++;
+		}
+		element[lastIndex] = o;
+
+		int anzahl = 0;
+		for(int x = 0; x < element.length; x++) {
+			if(element[x] != null) {
+				anzahl++;
+			}
+		}
+		if(anzahl > 1) {
+			for(int x = 0; x < anzahl-1; x++) {
+				if(element[x].compareTo(element[x+1]) < 0) {
+					Comparable temp1 = element[x];
+					Comparable temp2 = element[x+1];
+					element[x] = temp2;
+					element[x+1] = temp1;
+				}
+			}
 		}
 
 	}
 
+	public String toString() {
+		String ausgabe = "";
+
+		for (int x = 0; x < element.length; x++) {
+			if (element[x] != null) {
+				ausgabe = ausgabe + " " + element[x].toString();
+			}
+		}
+
+		return ausgabe;
+	}
 }
