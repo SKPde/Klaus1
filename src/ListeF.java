a342f5e109b372e0025927b2e6937f51abc54ca7
s.kuebel <s.kuebel@otterbach.de>
2017-08-10 13:01:13 +0200

    Erste Projektdaten mit vorgegebener Klasse

---
+++ src/ListeF.java
@@ -0,0 +1,16 @@
+
+public class ListeF {
+	private Comparable[] element; // Inhalte der Listenelemente
+	private int lastIndex; // Index des letzten Elements
+
+	public ListeF(int len) {
+		element = new Comparable[len];
+		lastIndex = -1; // leere Liste
+	}
+
+	public void sortedInsert(Comparable o) {
+		// sortiert einf ̈ugen
+
+	}
+
+}
